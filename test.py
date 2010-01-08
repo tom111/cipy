76bbe029c8e4e8dac241211b39301cfbf366e9fe
Thomas Kahle <tom111@gmx.de>
2010-01-08 17:58:26 +0100

    Simple test for CIMatrixEntry

--- test.py
+++ test.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python
 """A couple of tests"""
 
-print "Testing CIMatrix"
-from CIMatrix import CIMatrix
-A = CIMatrix(2,2)
-A.printRawList()
+#print "Testing CIMatrix"
+#from CIMatrix import CIMatrix
+#A = CIMatrix(2,2)
+#A.printRawList()
 
-print "\n\n Testing BinomialPattern222k"
-from BinomialPattern import BinomialPattern222k
-M = BinomialPattern222k(4)
-M.fromBinomial("1111*2222-2222*1212")
-N = BinomialPattern222k(4)
-N.fromBinomial("1111*1211-1211*2111")
+from CIMatrix import CIMatrixEntry
+a = CIMatrixEntry("0")
+b = CIMatrixEntry("-1")
 
-M.printAsMatrix()
-print "\n"
-N.printAsMatrix()
-print "\n"
-M.Spair(N).printAsMatrix()
+print a-b
+
