90f9d12b9e909fdd787ba93bf108aa36334628e3
Thomas Kahle <tom111@gmx.de>
2010-01-19 11:13:00 +0100

    222k Groebner Basis

--- 222kgb.py
+++ 222kgb.py
@@ -30,6 +30,11 @@ def reduceOnce (p, gb):
     chflag = False
     for g in gb:
         if p.redu(g) == 0:  # Note the side effect
+#            print "reduction succesfull, reducing move:"
+#            g.printAsMatrix()
+#            print "pattern after reduction:"
+#            p.printAsMatrix()
+#            print "Now fixing term order"
             chflag = True
             p.fixTermOrder()
     return chflag
@@ -46,29 +51,67 @@ def isZero (p):
                 return False
     return True
 
+def posSupport (g):
+    res = []
+    for i in range(4):
+        for j in range(2*g.dim2):
+            if g.matrix.getval(i,j).star : continue
+            if int(g.matrix.getval(i,j).s) <= 0 : continue
+            res.append( (i,j))
+    return res
+
+def feasible (g1,g2):
+    """ Determine whether an S-Pair will be reducible a priori!"""
+    p1 = posSupport(g1)
+    p2 = posSupport(g2)
+    if set(p1).intersection(set(p2)) == set(): return False
+    return True
+
+import pdb
+
 def gbcycle (gb):
-    # Construction of Spairs
+    # Smart construction of Spairs
     l = len(gb)
     spairs = []
+    spairsdebug = []
     for i in range(l):
         for j in (range(l-i-1)):
+            if not feasible(gb[i],gb[j+i+1]): continue
             spa = gb[i].Spair(gb[j+i+1])
             spa.fixTermOrder()
             spairs.append(spa)
+            spairsdebug.append((gb[i],gb[j+i+1]))
     # Reduction step
     for sp in spairs:
         reduceMax(sp,gb)
         if not isZero(sp):
             print "New Move:"
             sp.printAsMatrix()
+            print posSupport(sp)
             gb.append(sp)
+            if len(gb) == 28 : pdb.set_trace()
 
+gbcycle(currentgb)
+gbcycle(currentgb)
 gbcycle(currentgb)
 gbcycle(currentgb)
 
 print len(currentgb)
 
-currentgb[-1].printAsMatrix()
+c = currentgb[-1]
+c.printAsMatrix()
+
+sp = [c.Spair(g) for g in currentgb[:-1]]
+
+k=17
+print "Following is our example move"
+sp[k].printAsMatrix()
+reduceOnce(sp[k],currentgb)
+print "first reduction"
+sp[k].printAsMatrix()
+print "second reduction"
+reduceOnce(sp[k],currentgb)
+sp[k].printAsMatrix()
 
 exit(1)
 le = 0 
